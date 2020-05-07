fix continous space causal

algorithms/common_funcs.py
@@ -16,6 +16,9 @@ from ray.rllib.utils import try_import_tf
 
 from ray.rllib.utils.tf_ops import make_tf_callable
 
+from scipy.stats import norm
+
+
 CAUSAL_CONFIG = {"num_other_agents": 1,
                  "moa_weight": 10.0,
                  "train_moa_only_when_visible": True,
@@ -135,11 +138,26 @@ def compute_influence_reward(policy, trajectory):
     # extract out the probability under the actions we actually did take
     true_probs = trajectory[COUNTERFACTUAL_ACTIONS]
     traj_index = list(range(len(trajectory['obs'])))
-    true_probs = true_probs[traj_index, :, trajectory['actions'], :]
+    # print("traj actions", trajectory['actions'])
+    my_act = []
+    for i in trajectory['actions']:
+      val = int(i[0]*10)
+      if val<0:
+        # print(val)
+        val = 0
+
+      if val>9:
+        # print(val)
+        val = 9
+
+      my_act.append(val)
+    discrete = np.array(my_act)
+    # discrete = np.apply_along_axis(lambda x:int(x*10), 0, trajectory['actions'])
+    true_probs = true_probs[traj_index, :, discrete, :]
     true_probs = np.reshape(true_probs, [true_probs.shape[0], policy.num_other_agents, -1])
     true_probs = scipy.special.softmax(true_probs, axis=-1)
     true_probs = true_probs / true_probs.sum(axis=-1, keepdims=1)  # reduce numerical inaccuracies
-
+    # print("after true probs", true_probs)
     # Get marginal predictions where effect of self is marginalized out
     marginal_probs = marginalize_predictions_over_own_actions(policy, trajectory)  # [B, Num agents, Num actions]
 
@@ -198,24 +216,46 @@ def get_agent_visibility_multiplier(trajectory, num_other_agents, agent_ids):
 
 def marginalize_predictions_over_own_actions(policy, trajectory):
     # Probability of each action in original trajectory
-    action_probs = scipy.special.softmax(trajectory[ACTION_LOGITS], axis=-1)
-
+    # print("action logits", trajectory[ACTION_LOGITS])
+    mean, log_std = trajectory[ACTION_LOGITS][..., 0], trajectory[ACTION_LOGITS][..., 1]
+    # print("bth marginal", mean, log_std)
+    mean = mean.flatten() # as many means as
+    log_std = log_std.flatten()
+    std = np.exp(log_std)
+    n = mean.shape
+    # print(n)
+    # print(mean)
+    #
+    # dist = tfd.Normal(loc=mean, scale=std)
+    my_logits = []
+    for j in range(10):
+      # ar = np.array([j]*n)*0.1
+      # print("now printing")
+      my_logits.append(norm.pdf(j*0.1, mean, std))
+    # print(my_logits)
+    action_logits = np.concatenate(my_logits, axis=0).reshape(len(mean),1, 10)
+    # print(action_logits.shape)
+    # print("low of work", action_logits)
+    action_probs = scipy.special.softmax(action_logits, axis=-1)
+    # print("action probs", action_probs)
     # Normalize to reduce numerical inaccuracies
     action_probs = action_probs / action_probs.sum(axis=1, keepdims=1)
-
+    # print("action probs", action_probs)
     # Indexing of this is [B, Num agents, Agent actions, other agent logits] before we marginalize
     counter_probs = trajectory[COUNTERFACTUAL_ACTIONS]
+    # print("cunetr logits before", counter_probs)
     counter_probs = np.reshape(counter_probs, [counter_probs.shape[0], policy.num_other_agents, -1, action_probs.shape[-1]])
     counter_probs = scipy.special.softmax(counter_probs, axis=-1)
+    # print("cunetr logits after", counter_probs)
     marginal_probs = np.sum(counter_probs, axis=-2)
-
+    # print("marginal before", marginal_probs)
     # Multiply by probability of each action to renormalize probability
     tiled_probs = np.tile(action_probs, [1, policy.num_other_agents, 1])
     marginal_probs = np.multiply(marginal_probs, tiled_probs)
 
     # Normalize to reduce numerical inaccuracies
     marginal_probs = marginal_probs / marginal_probs.sum(axis=2, keepdims=1)
-
+    # print("marginal after", marginal_probs)
     return marginal_probs
 
 

models/watershed_moa_nets.py
@@ -6,13 +6,15 @@ from ray.rllib.models.model import restore_original_dimensions
 from ray.rllib.models.modelv2 import ModelV2
 from ray.rllib.models.tf.recurrent_tf_modelv2 import RecurrentTFModelV2
 from ray.rllib.utils.annotations import override
-from ray.rllib.utils import try_import_tf
+from ray.rllib.utils import try_import_tf, try_import_tfp
 from ray.rllib.policy.sample_batch import SampleBatch
 
 tf = try_import_tf()
 
+tfp = try_import_tfp()
+tfd = tfp.distributions
 # keras rnn Input - 16 - 16 - LSTM(64) - Out
-
+NUM_AGENTS = 4
 # moa lstm input -16-16 kerasrnn
 class KerasRNN(RecurrentTFModelV2):
     """Maps the input direct to an LSTM cell"""
@@ -137,7 +139,7 @@ class MOA_LSTM(RecurrentTFModelV2):
                                        model_config, name)
         try:
             self.id = model_config["custom_options"]["id"]
-            if self.id>=4:
+            if self.id>=NUM_AGENTS:
                 self.causal=True
             else:
                 self.causal = False
@@ -232,7 +234,7 @@ class MOA_LSTM(RecurrentTFModelV2):
 
         # First we have to compute it over the trajectory to give us the hidden state that we will actually use
         other_actions = input_dict["obs"]["other_agent_actions"]
-        agent_action = tf.cast(tf.expand_dims(input_dict["prev_action"], axis=-1), tf.float32)
+        agent_action = input_dict["prev_action"]
         stacked_actions = tf.concat([agent_action, other_actions], axis=-1)
         pass_dict = {"curr_obs": trunk, "prev_total_actions": stacked_actions}
 
@@ -242,13 +244,34 @@ class MOA_LSTM(RecurrentTFModelV2):
 
         # Now we can use that cell state to do the counterfactual predictions
         counterfactual_preds = []
-        for i in range(self.num_outputs):
-            possible_actions = np.array([i])[np.newaxis, np.newaxis, :]
+        counterfactual_pred_o, _, _ = self.moa_model.forward_rnn(pass_dict, [h2, c2], seq_lens)
+
+        for i in range(10):
+            possible_actions = np.array([i*0.1])[np.newaxis, np.newaxis, :]
             stacked_actions = tf.concat([possible_actions, other_actions], axis=-1)
             pass_dict = {"curr_obs": trunk, "prev_total_actions": stacked_actions}
-            counterfactual_pred, _, _ = self.moa_model.forward_rnn(pass_dict, [h2, c2], seq_lens)
-            counterfactual_preds.append(tf.expand_dims(counterfactual_pred, axis=-2))
-        self._counterfactual_preds = tf.concat(counterfactual_preds, axis=-2)
+            # print(self.moa_model.forward_rnn(pass_dict, [h2, c2], seq_lens))
+            # counterfactual_pred, _, _ = self.moa_model.forward_rnn(pass_dict, [h2, c2], seq_lens)
+            mean, log_std = tf.split(counterfactual_pred_o, 2, axis=2)
+            # print("bth", mean, log_std)
+            mean = tf.squeeze(mean)
+            log_std = tf.squeeze(log_std)
+            std = tf.exp(log_std)
+            # print("mean", mean)
+            # print("std", std)
+            dist = tfd.Normal(loc=mean, scale=std)
+            my_c = []
+            for j in range(10):
+              ar = np.array([j]*1)*0.1
+              # print("now printing")
+              my_c.append(dist.prob(ar))
+            counterfactual_pred = tf.concat(my_c,axis=0)
+            # print("low of work", counterfactual_pred)
+            counterfactual_preds.append(counterfactual_pred)
+        # print("out")
+        self._counterfactual_preds = tf.concat(counterfactual_preds, axis=0)
+        self._counterfactual_preds = tf.reshape(self._counterfactual_preds, [1,1,10, (NUM_AGENTS-1)*10])
+        # print(self._counterfactual_preds)
 
         # TODO(@evinitsky) move this into ppo_causal by using restore_original_dimensions()
         self._other_agent_actions = input_dict["obs"]["other_agent_actions"]
@@ -274,7 +297,7 @@ class MOA_LSTM(RecurrentTFModelV2):
 
         # stack the agent actions together
         other_agent_actions = tf.cast(obs_dict["other_agent_actions"], tf.float32)
-        agent_actions = tf.cast(tf.expand_dims(train_batch[SampleBatch.PREV_ACTIONS], axis=1), tf.float32)
+        agent_actions = train_batch[SampleBatch.PREV_ACTIONS]
         prev_total_actions = tf.concat([agent_actions, other_agent_actions], axis=-1)
 
         # Now we add the appropriate time dimension
@@ -300,7 +323,10 @@ class MOA_LSTM(RecurrentTFModelV2):
             i += 1
 
         moa_preds, _, _ = self.moa_model.forward_rnn(input_dict, states, train_batch.get("seq_lens"))
+
+
         return moa_preds
+
 
     def action_logits(self):
         return self._model_out

models/watershed_nets.py
@@ -10,7 +10,7 @@ from ray.rllib.utils.annotations import override
 from ray.rllib.utils import try_import_tf
 tf = try_import_tf()
 
-N_AGENTS = 4
+NUM_AGENTS = 4
 shared_layers = []
 ##FC
 # Input -16 -16 out
@@ -20,7 +20,7 @@ shared_layers = []
 #Input -16 -16 LSTM(64) - out
 #Value Input -16 -16 LSTM(64) - out
 
-for i in range(N_AGENTS):
+for i in range(NUM_AGENTS):
     shared_layers.append(tf.keras.layers.Dense(
         16,
         name="my_layer1"+str(i),
@@ -39,7 +39,7 @@ class FCNet(TFModelV2):
             id = model_config["custom_options"]["id"]
 
             self.id = model_config["custom_options"]["id"]
-            if self.id>=4:
+            if self.id>=NUM_AGENTS:
                 self.causal=True
             else:
                 self.causal = False
@@ -108,8 +108,15 @@ class LSTMFCNet(RecurrentTFModelV2):
         try:
             share_comm_layer = model_config["custom_options"]["share_comm_layer"]
             id = model_config["custom_options"]["id"]
-        except:
+            self.id = model_config["custom_options"]["id"]
+            if self.id>=NUM_AGENTS:
+                self.causal=True
+            else:
+                self.causal = False
+        except Exception as e:
+            print(e)
             share_comm_layer = False
+            self.causal = False
         # Define input layers
         input_layer = tf.keras.layers.Input(
             shape=(None, obs_space.shape[0]), name="inputs")